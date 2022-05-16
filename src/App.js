37ff72be49c61fe118ee36626bbcab82059072e4
jinwoo <jinwoo3158@gmail.com>
2022-05-16 19:08:10 +0900

    0516 second commit

--- src/App.js
+++ src/App.js
@@ -4,33 +4,68 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import bg from "./img/bg.png";
 import data from "./data.js";
 import React, { useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./routes/Detail.js";
 
 function App() {
   let [shoes] = useState(data);
+  let navigate = useNavigate();
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#cart">Cart</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/");
+                // navigate(-1)뒤로가기;
+                // navigate(1)앞으로가기;
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              Detail
+            </Nav.Link>
+            {/* <Link to="/">Home</Link>
+            <Link to="/detail">Cart </Link> */}
           </Nav>
         </Container>
       </Navbar>
-
-      <div
-        className="main-bg"
-        style={{ backgroundImage: "url(" + bg + ")" }}
-      ></div>
-      <div className="container">
-        <div className="row">
-          {shoes.map((a, i) => {
-            return <Card shoes={shoes[i]} i={i + 1} />;
-          })}
-        </div>
-      </div>
+      <Routes>
+        <Route
+          path="/detail"
+          element={
+            <div>
+              <Detail></Detail>
+            </div>
+          }
+        />
+        <Route path="/about" element={<div>어바웃페이지임</div>} />
+        <Route
+          path="/"
+          element={
+            <div>
+              <div
+                className="main-bg"
+                style={{ backgroundImage: "url(" + bg + ")" }}
+              ></div>
+              <div className="container">
+                <div className="row">
+                  {shoes.map((a, i) => {
+                    return <Card shoes={shoes[i]} i={i + 1} />;
+                  })}
+                </div>
+              </div>
+            </div>
+          }
+        />
+      </Routes>
     </div>
   );
 }
