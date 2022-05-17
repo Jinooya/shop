eba73f41bf3515594e1784082c7c00f244257e74
jinwoo <jinwoo3158@gmail.com>
2022-05-17 17:12:30 +0900

    newbalance

--- src/App.js
+++ src/App.js
@@ -1,40 +1,62 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import {
+  Navbar,
+  Container,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 import bg from "./img/bg.png";
 import data from "./data.js";
 import React, { useState } from "react";
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import Detail from "./routes/Detail.js";
+import axios from "axios";
 
 function App() {
-  let [shoes] = useState(data);
+  let [shoes, setShoes] = useState(data);
   let navigate = useNavigate();
+  let [more, setMore] = useState(2);
+
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Navbar.Brand
+            className="logo"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            NewBalance
+          </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link
               onClick={() => {
                 navigate("/");
-                // navigate(-1)뒤로가기;
-                // navigate(1)앞으로가기;
               }}
             >
-              Home
+              HOME
             </Nav.Link>
             <Nav.Link
               onClick={() => {
-                navigate("/detail");
+                navigate("/cart");
               }}
             >
-              Detail
+              CART
             </Nav.Link>
-            {/* <Link to="/">Home</Link>
-            <Link to="/detail">Cart </Link> */}
           </Nav>
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
         </Container>
       </Navbar>
       <Routes>
@@ -42,10 +64,11 @@ function App() {
           path="/detail"
           element={
             <div>
-              <Detail></Detail>
+              <Detail shoes={shoes}></Detail>
             </div>
           }
         />
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
         <Route path="/about" element={<div>어바웃페이지임</div>} />
         <Route
           path="/"
@@ -56,20 +79,57 @@ function App() {
                 style={{ backgroundImage: "url(" + bg + ")" }}
               ></div>
               <div className="container">
+                <h4>brand new shoes 👟</h4>
                 <div className="row">
                   {shoes.map((a, i) => {
-                    return <Card shoes={shoes[i]} i={i + 1} />;
+                    return (
+                      <Card
+                        key={i}
+                        navigate={navigate}
+                        shoes={shoes[i]}
+                        i={i + 1}
+                      />
+                    );
                   })}
                 </div>
               </div>
+
+              <button
+                className="moreBtn"
+                onClick={() => {
+                  axios
+                    .get(`https://codingapple1.github.io/shop/data${more}.json`)
+                    .then((결과) => {
+                      let copy = [...shoes, ...결과.data];
+                      setShoes(copy);
+                    });
+                  setMore(more--);
+                  console.log(more);
+                }}
+              >
+                더보기 +
+              </button>
+
+              <footer>
+                <h1>ShoeShop</h1>
+                <p>call 010-5032-3572</p>
+              </footer>
             </div>
           }
         />
+        <Route path="*" element={<div>준비중인 페이지</div>} />
       </Routes>
     </div>
   );
 }
-
+function Event() {
+  return (
+    <div>
+      <h3>오늘의 이벤트</h3>
+      <Outlet></Outlet>
+    </div>
+  );
+}
 function Card(props) {
   return (
     <div className="col-md-4">
@@ -77,7 +137,14 @@ function Card(props) {
         src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"}
         width="80%"
       />
-      <h4> {props.shoes.title} </h4>
+      <h4
+        className="shoesTitle"
+        onClick={() => {
+          props.navigate("/detail/0");
+        }}
+      >
+        {props.shoes.title}
+      </h4>
       <p> {props.shoes.price} </p>
       <p> {props.shoes.content} </p>
     </div>
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,17 +1,60 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+let YellowBtn = styled.button`
+  background: ${(props) => props.bg};
+  color: ${(props) => (props.bg == "blue" ? "white" : "black")};
+  padding: 10px;
+`;
+
+let Box = styled.button`
+  background: yellow;
+  color: black;
+  padding: 20px;
+`;
+
 function Detail(props) {
+  useEffect(() => {
+    //useEffect안의 코드는 html렌더링 후에 동작합니다.
+    let a = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(a);
+    };
+  });
+
+  let { id } = useParams();
+  let 찾은상품 = props.shoes.find(function (x) {
+    return x.id == id;
+  });
+
+  let [alert, setAlert] = useState(true);
+
   return (
     <div className="container">
+      {alert == true ? (
+        <div className="alert alert-warning">2초이내 구매시 할인</div>
+      ) : null}
+
+      {/* <Box>
+        <YellowBtn bg="blue">버튼</YellowBtn>
+        <YellowBtn bg="orange">버튼</YellowBtn>
+      </Box> */}
+
       <div className="row">
         <div className="col-md-6">
           <img
-            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            src={"https://codingapple1.github.io/shop/shoes1.jpg"}
             width="100%"
           />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p> {찾은상품.content}</p>
+          <p> {찾은상품.price}</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
