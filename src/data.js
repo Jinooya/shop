4b2ad52affcae7d7c708250336942badcb8bf311
jinwoo <jinwoo3158@gmail.com>
2022-05-27 10:40:07 +0900

    0527 commit

--- src/data.js
+++ src/data.js
@@ -4,9 +4,9 @@ let data = [
     title: "White and Black",
     content: "Born in France",
     price: 120000,
-    info: "흠",
-    rev: "흠2",
-    qna: "흠3",
+    info: "90년대 트레이닝 카테고리의 WX452 모델을 재해석해 개발된 트레이닝화입니다",
+    rev: "패밀리화로 구매했는데 색상도 잘 어울리고 편하고 좋아요",
+    qna: "배송문의",
   },
 
   {
